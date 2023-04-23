72986c74018260a699b09ed79b98dc25965fec46
palashyerpude <128513490+palashyerpude@users.noreply.github.com>
2023-04-23 12:09:16 +0530

    app.vue
    app.vue is the main Vue.js component for a Vue.js application

---
+++ src/app.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="./assets/Vue.js_Logo_2.svg.png" /> 
+   <TodoApp msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import TodoApp from './components/TodoApp.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    TodoApp
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
