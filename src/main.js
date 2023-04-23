fe32a6e7356dab3896b6702895afc798cb6c792c
palashyerpude <128513490+palashyerpude@users.noreply.github.com>
2023-04-23 12:17:01 +0530

    main.js
    import create app from Vue.js

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './app.vue'
+
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
+createApp(App).mount('#app')
