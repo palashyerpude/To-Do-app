45a2fb4f8af50cfe1cdf8b52cafa4d50666d057b
palashyerpude <128513490+palashyerpude@users.noreply.github.com>
2023-04-23 12:18:03 +0530

    Delete main.js

--- public/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './app.vue'
-
-// import 'bootstrap/dist/css/bootstrap.css'
-// import 'bootstrap-vue/dist/bootstrap-vue.css'
-createApp(App).mount('#app')
